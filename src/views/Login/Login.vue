cebb17794420405b1fc5e06c79bc2973273ec31b
jingay08 <836505655@qq.com>
2020-11-16 14:38:20 +0800

    更新登陆页

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        <div class="">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import './style.scss';
+export default {
+    name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
