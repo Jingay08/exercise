80fcb0ec1d25f696307ee050b114a0e7b4faa3d5
jingay08 <836505655@qq.com>
2020-11-20 11:39:16 +0800

    add：树结构

---
+++ src/components/Tree/Tree.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="Tree">
+        <ul v-for="(item, idx) in data" :key="idx"  :class="`${classTitle}`" class="tree-item">
+            <li @click="expend($event,item)">
+                <i :class="mathRandom()" class="preIcon"></i>
+                <span class="title">{{item.name}}</span>
+                <template  v-if="item.children">
+                    <i class="el-icon-arrow-down arrowDown"></i>
+                </template> 
+            </li>
+            <template v-if="item.children">
+                <collapse-transition>
+                    <Tree :data="item.children" :classTitle="classTitle + '-' + idx" class="aa" v-show="item.show"/>
+                </collapse-transition>
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+import './style.scss';
+export default {
+    name: "Tree",
+    props: {
+        data: {
+           
+        },
+        classTitle: {
+            default:() => {
+                return 'item';
+            }
+        }
+    },
+    data() {
+        return {
+            randomIcon: this.constant.randomIcon
+        }
+    },
+    methods:{
+        mathRandom() {
+            const randomItem = this.randomIcon[Math.floor(Math.random() * this.randomIcon.length)];
+            return randomItem;
+        },
+        expend(e, item) {
+            let tarDom = e.currentTarget;
+            let resultIsHave = tarDom.querySelector('.arrowDown');
+            if(resultIsHave) {
+                console.log(item, '__')
+                item.show = !item.show;
+            } else {
+                console.log(222)
+            }
+         
+          
+        }
+    }
+}
+</script>
