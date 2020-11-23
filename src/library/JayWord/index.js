804b7fcead298a600acebbe5082e0551138b9057
jingay08 <836505655@qq.com>
2020-11-23 21:18:58 +0800

    update

---
+++ src/library/JayWord/index.js
@@ -0,0 +1,8 @@
+import Els from '../Els';
+import Vue from 'vue';
+class JayWord extends Els{
+    constructor() {
+        super();
+    }
+}
+export default JayWord;
---
+++ src/views/Tailor/Tailor.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="Tailor">
+       <transition name="fade">
+            <Header v-if="headerFlag" :backChunkFlag="backFlag"/>
+        </transition>
+        <div class="day index-back"></div>
+        <div class="night index-back"></div>
+    </div>
+</template>
+
+<script>
+import './style.scss';
+export default {
+    name: 'Tailor',
+    data() {
+        return {
+            headerFlag: false,
+            backFlag: true,
+        }
+    },
+    methods:{
+        Move() {
+            document.onmousemove = (e) => {
+                if(e.pageY < 10) {
+                    this.headerFlag = true;
+                    return;
+                } 
+                if(e.pageY > 60) this.headerFlag = false; 
+                
+            }
+        }
+    },
+    mounted() {
+        this.Move()
+    }
+}
+</script>
